57c7d60f2241fc2dc41eeb9e4f512533f868a8d8
DucHuy74 <duchuyhl74205@gmail.com>
2025-01-04 22:16:51 +0700

    add project

---
+++ NotesModule/src/main/java/com/project/notesmodule/NotesModuleApplication.java
@@ -0,0 +1,13 @@
+package com.project.notesmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesModuleApplication.class, args);
+    }
+
+}
