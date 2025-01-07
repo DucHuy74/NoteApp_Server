fb3aa96647504b7b4d59a07ef43b687633c5278b
DucHuy74 <duchuyhl74205@gmail.com>
2025-01-07 21:21:11 +0700

    Folder CRUD

---
+++ FolderModule/src/main/java/com/project/foldermodule/dto/request/FolderCreationRequest.java
@@ -0,0 +1,17 @@
+package com.project.foldermodule.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+public class FolderCreationRequest {
+    @NotBlank(message = "Folder name must not be blank")
+    @Size(max = 250, message = "Folder name must not exceed 250 characters")
+    private String folderName;
+    private LocalDate creatdate;
+    private LocalDate updateAt;
+}
